9943c14c711a69cd0422b23075808c431639617e
Brett Keck <bkeck@elevenfiftyconsulting.com>
2016-09-16 13:35:51 -0400

    Fixed Silver app

--- Silver/PeopleMon/PeopleMon/View Controllers/MapViewController.swift
+++ Silver/PeopleMon/PeopleMon/View Controllers/MapViewController.swift
@@ -20,7 +20,7 @@ class MapViewController: UIViewController, SegueHandlerType {
     var overlay: MKOverlay?
     var firstLocation = true
     
-    var timer: NSTimer?
+    var timer: Timer?
     
     var nearbyView: UIView!
     var nearbyCollectionView: UICollectionView!
@@ -41,8 +41,8 @@ class MapViewController: UIViewController, SegueHandlerType {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
         mapView.showsUserLocation = true
-        mapView.zoomEnabled = false
-        mapView.scrollEnabled = false
+        mapView.isZoomEnabled = false
+        mapView.isScrollEnabled = false
         locationManager.startUpdatingLocation()
         
         mapView.delegate = self
@@ -56,9 +56,9 @@ class MapViewController: UIViewController, SegueHandlerType {
         super.didReceiveMemoryWarning()
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         if !WebServices.shared.userAuthTokenExists() || WebServices.shared.userAuthTokenExpired() {
-            performSegueWithIdentifier(.PresentLoginNoAnimation, sender: self)
+            performSegueWithIdentifier(segueIdentifier: .PresentLoginNoAnimation, sender: self)
         } else {
             let infoUser = User()
             WebServices.shared.getObject(infoUser, completion: { (user, error) in
@@ -69,8 +69,8 @@ class MapViewController: UIViewController, SegueHandlerType {
         }
     }
     
-    override func viewWillDisappear(animated: Bool) {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
         stopTimer()
     }
     
@@ -98,7 +98,7 @@ class MapViewController: UIViewController, SegueHandlerType {
     }
     
     func startTimer() {
-        timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(loadMap), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(loadMap), userInfo: nil, repeats: true)
     }
     
     func stopTimer() {
@@ -107,15 +107,15 @@ class MapViewController: UIViewController, SegueHandlerType {
     }
     
     func setupNearbyView() {
-        let bounds = UIScreen.mainScreen().bounds
+        let bounds = UIScreen.main.bounds
         let width: CGFloat = 196
         let height: CGFloat = 240
         
         let closeButton = UIButton(frame: CGRect(x: 8, y: 196, width: 180, height: 44))
-        closeButton.addTarget(self, action: #selector(closeNearbyView(_:)), forControlEvents: .TouchUpInside)
-        closeButton.setTitle("Close", forState: .Normal)
-        closeButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        closeButton.titleLabel?.font = UIFont.boldSystemFontOfSize(14)
+        closeButton.addTarget(self, action: #selector(closeNearbyView(sender:)), for: .touchUpInside)
+        closeButton.setTitle("Close", for: .normal)
+        closeButton.setTitleColor(UIColor.blue, for: .normal)
+        closeButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         nearbyView = UIView(frame: CGRect(x: (bounds.width - width) / 2.0, y: (bounds.height - height) / 2.0, width: width, height: height))
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 60, height: 60)
@@ -125,15 +125,15 @@ class MapViewController: UIViewController, SegueHandlerType {
         nearbyCollectionView = UICollectionView(frame: CGRect(x: 8, y: 8, width: 180, height: 180), collectionViewLayout: layout)
         nearbyCollectionView.dataSource = self
         nearbyCollectionView.delegate = self
-        nearbyCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
-        nearbyCollectionView.scrollEnabled = false
+        nearbyCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        nearbyCollectionView.isScrollEnabled = false
         nearbyView.addSubview(nearbyCollectionView)
         nearbyView.addSubview(closeButton)
-        nearbyView.backgroundColor = UIColor.whiteColor()
-        nearbyCollectionView.backgroundColor = UIColor.clearColor()
+        nearbyView.backgroundColor = UIColor.white
+        nearbyCollectionView.backgroundColor = UIColor.clear
         self.view.addSubview(nearbyView)
         
-        let translateTransform = CGAffineTransformMakeTranslation(0, bounds.height)
+        let translateTransform = CGAffineTransform(translationX: 0, y: bounds.height)
         nearbyView.transform = translateTransform
     }
     
@@ -141,7 +141,7 @@ class MapViewController: UIViewController, SegueHandlerType {
     // MARK: - IBActions
     @IBAction func logout(sender: AnyObject) {
         UserStore.shared.logout { 
-            self.performSegueWithIdentifier(.PresentLogin, sender: self)
+            self.performSegueWithIdentifier(segueIdentifier: .PresentLogin, sender: self)
         }
     }
     
@@ -151,31 +151,38 @@ class MapViewController: UIViewController, SegueHandlerType {
             if let objects = objects {
                 self.nearbyPeople = objects
                 for person in objects {
-                    if let lat = person.latitude, long = person.longitude {
-                        person.distance = self.locationManager.location?.distanceFromLocation(CLLocation(latitude: lat, longitude: long))
+                    if let lat = person.latitude, let long = person.longitude {
+                        person.distance = self.locationManager.location?.distance(from: CLLocation(latitude: lat, longitude: long))
                     }
                 }
-                self.nearbyPeople = objects.sort({ (person1, person2) -> Bool in
-                    person1.distance < person2.distance
+                self.nearbyPeople = objects.sorted(by: { (person1, person2) -> Bool in
+                    switch (person1.distance, person2.distance) {
+                    case let (distance1?, distance2?):
+                        return distance1 < distance2
+                    case (nil, _?):
+                        return true
+                    default:
+                        return false
+                    }
                 })
                 self.nearbyCollectionView.reloadData()
             }
         }
-        UIView.animateWithDuration(0.8, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: { 
-            self.nearbyView.transform = CGAffineTransformMakeTranslation(0, 0)
+        UIView.animate(withDuration: 0.8, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: { 
+            self.nearbyView.transform = CGAffineTransform(translationX: 0, y: 0)
             }, completion: nil)
     }
     
     @IBAction func closeNearbyView(sender: AnyObject) {
-        UIView.animateWithDuration(0.8, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
-            self.nearbyView.transform = CGAffineTransformMakeTranslation(0, UIScreen.mainScreen().bounds.height)
+        UIView.animate(withDuration: 0.8, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
+            self.nearbyView.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height)
             }, completion: nil)
     }
 }
 
 // MARK: - CLLocationManagerDelegate
 extension MapViewController: CLLocationManagerDelegate {
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last!
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpanMake(latitudeDelta, longitudeDelta))
@@ -183,44 +190,43 @@ extension MapViewController: CLLocationManagerDelegate {
         self.mapView.setRegion(region, animated: true)
         
         if let userOverlay = userOverlay {
-            mapView.removeOverlay(userOverlay)
+            mapView.remove(userOverlay)
         }
-        userOverlay = MKCircle(centerCoordinate: center, radius: Constants.radius)
-        mapView.addOverlay(userOverlay!)
+        userOverlay = MKCircle(center: center, radius: Constants.radius)
+        mapView.add(userOverlay!)
     }
 }
 
 
 // MARK: - MKMapViewDelegate
 extension MapViewController: MKMapViewDelegate {
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             let userPin = "userLocation"
-            var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(userPin)
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: userPin)
             if pinView == nil {
                 pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: userPin)
                 pinView?.canShowCallout = false
             } else {
                 pinView?.annotation = annotation
             }
-            if let image = Utils.imageFromString(UserStore.shared.user?.avatar) {
-                let resizedImage = Utils.resizeImage(image, maxSize: Constants.pinImageSize)
+            if let image = Utils.imageFromString(imageString: UserStore.shared.user?.avatar) {
+                let resizedImage = Utils.resizeImage(image: image, maxSize: Constants.pinImageSize)
                 pinView?.image = resizedImage
                 pinView?.layer.cornerRadius = 16
-                pinView?.contentMode = .ScaleAspectFill
+                pinView?.contentMode = .scaleAspectFill
                 pinView?.clipsToBounds = true
                 pinView?.layer.borderWidth = 2
-                pinView?.layer.borderColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1).CGColor
+                pinView?.layer.borderColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1).cgColor
             } else {
                 pinView?.image = Images.Avatar.image()
             }
-            
             return pinView
         }
         
         let reuseId = "pin"
         
-        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
         if pinView == nil {
             pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView?.canShowCallout = false
@@ -229,11 +235,11 @@ extension MapViewController: MKMapViewDelegate {
         }
         
         if let mapPin = annotation as? MapPin {
-            if let image = Utils.imageFromString(mapPin.person?.avatar) {
-                let resizedImage = Utils.resizeImage(image, maxSize: Constants.pinImageSize)
+            if let image = Utils.imageFromString(imageString: mapPin.person?.avatar) {
+                let resizedImage = Utils.resizeImage(image: image, maxSize: Constants.pinImageSize)
                 pinView?.image = resizedImage
                 pinView?.layer.cornerRadius = Constants.pinImageSize / 2.0
-                pinView?.contentMode = .ScaleAspectFill
+                pinView?.contentMode = .scaleAspectFill
                 pinView?.clipsToBounds = true
             } else {
                 pinView?.image = Images.Avatar.image()
@@ -243,29 +249,29 @@ extension MapViewController: MKMapViewDelegate {
         return pinView
     }
     
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-        if let mapPin = view.annotation as? MapPin, person = mapPin.person, name = person.username, userId = person.userId {
-            let alert = UIAlertController(title: "Catch User", message: "Catch \(name)?", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Catch", style: .Default, handler: { (action) in
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let mapPin = view.annotation as? MapPin, let person = mapPin.person, let name = person.username, let userId = person.userId {
+            let alert = UIAlertController(title: "Catch User", message: "Catch \(name)?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Catch", style: .default, handler: { (action) in
                 let catchPerson = Person(userId: userId, radius: Constants.radius)
                 WebServices.shared.postObject(catchPerson, completion: { (object, error) in
                     if let error = error {
-                        self.presentViewController(Utils.createAlert(message: error), animated: true, completion: nil)
+                        self.present(Utils.createAlert(message: error), animated: true, completion: nil)
                     } else {
-                        self.presentViewController(Utils.createAlert("Catch", message: "User Caught", dismissButtonTitle: "OK"), animated: true, completion: nil)
+                        self.present(Utils.createAlert(title: "Catch", message: "User Caught", dismissButtonTitle: "OK"), animated: true, completion: nil)
                     }
                 })
             }))
-            alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
-    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let circleOverlay = MKCircleRenderer(overlay: overlay)
         circleOverlay.lineWidth = 1
-        circleOverlay.strokeColor = UIColor.lightGrayColor()
-        circleOverlay.fillColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.5)
+        circleOverlay.strokeColor = UIColor.lightGray
+        circleOverlay.fillColor = UIColor.lightGray.withAlphaComponent(0.5)
         return circleOverlay
     }
 }
@@ -274,8 +280,8 @@ extension MapViewController: MKMapViewDelegate {
 // MARK: - UserStoreDelegate
 extension MapViewController: UserStoreDelegate {
     func userLoggedIn() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(stopTimer), name: UIApplicationWillResignActiveNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(startTimer), name: UIApplicationDidBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(stopTimer), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(startTimer), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
         stopTimer()
         startTimer()
     }
@@ -284,22 +290,21 @@ extension MapViewController: UserStoreDelegate {
 
 // MARK: - CollectionView
 extension MapViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    private func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return min(nearbyPeople.count, 9)
     }
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath)
         
         let person = nearbyPeople[indexPath.row];
-        print("row: \(indexPath.row), distance: \(person.distance)")
         let imageView = CircleImage(frame: CGRect(x: 5, y: 5, width: 50, height: 50))
-        imageView.contentMode = .ScaleAspectFill
-        imageView.image = Utils.imageFromString(person.avatar)
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = Utils.imageFromString(imageString: person.avatar)
         cell.addSubview(imageView)
         
         return cell
